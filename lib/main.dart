eec57a10b4dc8b81a02b22d1812c1953454d3da8
GComputeNerd <GComputeNerd@users.noreply.github.com>
2024-01-31 14:16:15 +0530

    Added Input Box Widget

--- lib/main.dart
+++ lib/main.dart
@@ -21,46 +21,113 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key, required this.title});
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+
     return Scaffold(
-      backgroundColor: Color(0xFF263238),
+      backgroundColor: const Color(0xFF263238),
       body: Center(
         child: Column(
           children: [
             Container(
+              height: height*0.04,
+              margin: const EdgeInsets.only(top: 40),
               child: Image.asset(
                 'lib/icons/sedan.png',
                 fit: BoxFit.fitHeight,
               ),
-              height: 40,
-              margin: EdgeInsets.only(top: 40),
             ),
-            Text(
+            const Text(
               "Mileage",
               style: TextStyle(
                 fontFamily: 'Iceland',
-                fontSize: 52,
+                fontSize: 40,
                 color: Colors.white,
               ),),
+            InputBox(
+              width: width*0.8,
+              hintText: "Distance",
+              suffix: "km",
+            ),
+            const SizedBox(height: 5,),
+            const Text(
+              "per",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 19
+              ),
+            ),
+            const SizedBox(height: 5,),
+            InputBox(
+              width: width*0.8, 
+              hintText: "Fuel", 
+              suffix: "ltr"
+            ),
+            const SizedBox(height: 13,),
+            InputBox(
+              width: width*0.8, 
+              hintText: "Speed", 
+              suffix: "km/hr"
+            )
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class InputBox extends StatelessWidget {
+  const InputBox({
+    super.key,
+    required this.width,
+    required this.hintText,
+    required this.suffix
+  });
+
+  final String hintText;
+  final String suffix;
+
+  final double width;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: width,
+      child: TextField(
+        cursorColor: Colors.white10,
+        keyboardType: TextInputType.number,
+        decoration: InputDecoration(
+          focusedBorder: const OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(20)),
+            borderSide: BorderSide(
+              color: Color(0xFF1B8A62),
+              width: 2.0,
+            ),
+          ),
+          enabledBorder: const OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(20)),
+            borderSide: BorderSide(color: Color(0xAA8C7B91), width: 2.0),
+          ),
+          isDense: true,
+          contentPadding: const EdgeInsets.symmetric(vertical: 12,horizontal: 18),
+          hintText: hintText,
+          hintStyle: const TextStyle(
+            fontFamily: "Iceland",
+            fontSize: 20,
+            color: Colors.white
+          ),
+          suffix: Text(suffix),
+        ),
+        style: const TextStyle(
+          fontFamily: "Iceland",
+          fontSize: 20,
+          color: Colors.white
         ),
       ),
     );
